d0d42215057557f89db9509d2b1c5df3ba7ba96f
JINOJ37 <jinoj0423@gmail.com>
2024-01-02 19:04:00 +0900

    :sparkles: Feat : club root page 구현
    클럽 루트 페이지 생성 및 간단 구현

---
+++ dplanner/lib/pages/club_my_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClubMyPage extends StatelessWidget {
+  const ClubMyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ dplanner/lib/pages/club_timetable_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClubTimetablePage extends StatelessWidget {
+  const ClubTimetablePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
