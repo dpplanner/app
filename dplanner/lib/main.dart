223e96981183f71ea59f27c71750cefc738d8b3e
JINOJ37 <jinoj0423@gmail.com>
2024-01-09 02:53:31 +0900

    :sparkles: Feat : 페이지 관리 (#3)
    routes.dart 생성

--- dplanner/lib/main.dart
+++ dplanner/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:dplanner/club_root_page.dart';
+import 'package:dplanner/routes.dart';
+import 'package:dplanner/style.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'DPlanner',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: AppColor.objectColor,
         useMaterial3: true,
       ),
-      home: const ClubRootPage(),
+      initialRoute: '/',
+      getPages: page,
     );
   }
 }
