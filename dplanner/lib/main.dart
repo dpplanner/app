af45d02d6db9d97d7e64a667a34fe52a55a3eaad
JINOJ37 <jinoj0423@gmail.com>
2024-01-09 20:01:33 +0900

    :sparkles: Feat : Splash Page 구현 (#4)
    flutter_native_splash 패키지 사용

--- dplanner/lib/main.dart
+++ dplanner/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:dplanner/routes.dart';
 import 'package:dplanner/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
