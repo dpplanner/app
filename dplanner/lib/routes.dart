417f93af67e903ddcdf016284687384b192065c4
JINOJ37 <jinoj0423@gmail.com>
2024-01-10 01:32:56 +0900

    :sparkles: Feat : Login Page 구현 (#5)
    login_page.dart 생성

--- dplanner/lib/routes.dart
+++ dplanner/lib/routes.dart
@@ -2,11 +2,11 @@ import 'package:dplanner/club_root_page.dart';
 import 'package:dplanner/pages/club_home_page.dart';
 import 'package:dplanner/pages/club_my_page.dart';
 import 'package:dplanner/pages/club_timetable_page.dart';
-import 'package:dplanner/pages/splash_page.dart';
+import 'package:dplanner/pages/login_page.dart';
 import 'package:get/get.dart';
 
 final page = [
-  GetPage(name: '/', page: () => const SplashPage()),
+  GetPage(name: '/', page: () => const LoginPage()),
   //GetPage(name: '/', page: () => const ClubRootPage()),
   GetPage(name: '/tab1', page: () => const ClubTimetablePage()),
   GetPage(name: '/tab2', page: () => const ClubHomePage()),
