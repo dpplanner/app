652b7448008c0f8d85cd9d275812c514e55eb920
JINOJ37 <jinoj0423@gmail.com>
2024-01-12 04:29:36 +0900

    :sparkles: Feat : 클럽 리스트 화면 구현 (#6)
    club_list_page.dart 생성

--- dplanner/lib/routes.dart
+++ dplanner/lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:dplanner/club_root_page.dart';
 import 'package:dplanner/pages/club_home_page.dart';
+import 'package:dplanner/pages/club_list_page.dart';
 import 'package:dplanner/pages/club_my_page.dart';
 import 'package:dplanner/pages/club_timetable_page.dart';
 import 'package:dplanner/pages/login_page.dart';
@@ -7,7 +8,8 @@ import 'package:get/get.dart';
 
 final page = [
   GetPage(name: '/', page: () => const LoginPage()),
-  //GetPage(name: '/', page: () => const ClubRootPage()),
+  GetPage(name: '/club_list', page: () => const ClubListPage()),
+  GetPage(name: '/club_root', page: () => const ClubRootPage()),
   GetPage(name: '/tab1', page: () => const ClubTimetablePage()),
   GetPage(name: '/tab2', page: () => const ClubHomePage()),
   GetPage(name: '/tab3', page: () => const ClubMyPage())
