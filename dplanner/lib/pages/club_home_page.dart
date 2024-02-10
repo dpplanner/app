32187fd0247c8780ab903b3d4640a965f00a7796
JINOJ37 <jinoj0423@gmail.com>
2024-02-10 22:46:38 +0900

    :sparkles: Feat : 공용 바텀 네비게이션 바 생성 (#20)
    bottom_bar.dart 생성 및 club_root_page.dart 삭제

--- dplanner/lib/pages/club_home_page.dart
+++ dplanner/lib/pages/club_home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_sfsymbols/flutter_sfsymbols.dart';
 
 import '../style.dart';
+import '../widgets/bottom_bar.dart';
 import '../widgets/outline_textform.dart';
 import '../widgets/post_card.dart';
 
@@ -20,101 +21,98 @@ class _ClubHomePageState extends State<ClubHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.backgroundColor2,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60.0),
-        child: AppBar(
-          backgroundColor: AppColor.backgroundColor,
-          title: Padding(
-            padding:
-                EdgeInsets.only(left: SizeController.to.screenWidth * 0.05),
-            child: Row(
-              children: [
-                const Text(
-                  "Dance P.O.zz",
-                  style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    SFSymbols.info_circle,
-                    color: AppColor.textColor,
+        backgroundColor: AppColor.backgroundColor2,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60.0),
+          child: AppBar(
+            backgroundColor: AppColor.backgroundColor,
+            title: Padding(
+              padding:
+                  EdgeInsets.only(left: SizeController.to.screenWidth * 0.05),
+              child: Row(
+                children: [
+                  const Text(
+                    "Dance P.O.zz",
+                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
                   ),
-                )
-              ],
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      SFSymbols.info_circle,
+                      color: AppColor.textColor,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Padding(
-                padding: EdgeInsets.only(
-                    right: SizeController.to.screenWidth * 0.05),
-                child: const Icon(
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
                   SFSymbols.bell,
                   color: AppColor.textColor,
                 ),
               ),
-            )
-          ],
-          automaticallyImplyLeading: false,
-        ),
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: AppColor.backgroundColor,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(18, 0, 18, 0),
-                  child: Form(
-                      key: _formKey,
-                      child: OutlineTextForm(
-                        hintText: '게시글 제목, 내용, 작성자를 검색해보세요',
-                        controller: searchPost,
-                        isColored: true,
-                        icon: const Icon(
-                          SFSymbols.search,
-                          color: AppColor.textColor2,
-                        ),
-                      )),
-                ),
-              ),
-              Container(
-                color: AppColor.backgroundColor,
-                height: SizeController.to.screenHeight * 0.01,
-              ),
-              const PostCard(),
-              Container(
-                height: SizeController.to.screenHeight * 0.01,
-              ),
-              const PostCard(),
-              Container(
-                height: SizeController.to.screenHeight * 0.01,
-              ),
-              const PostCard(),
-              Container(
-                height: SizeController.to.screenHeight * 0.01,
-              ),
-              const PostCard(),
+              SizedBox(width: SizeController.to.screenWidth * 0.05)
             ],
+            automaticallyImplyLeading: false,
           ),
         ),
-      ),
-      floatingActionButton: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: AppColor.objectColor,
-          shape: const CircleBorder(),
-          padding: const EdgeInsets.all(15),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: AppColor.backgroundColor,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(18, 0, 18, 0),
+                    child: Form(
+                        key: _formKey,
+                        child: OutlineTextForm(
+                          hintText: '게시글 제목, 내용, 작성자를 검색해보세요',
+                          controller: searchPost,
+                          isColored: true,
+                          icon: const Icon(
+                            SFSymbols.search,
+                            color: AppColor.textColor2,
+                          ),
+                        )),
+                  ),
+                ),
+                Container(
+                  color: AppColor.backgroundColor,
+                  height: SizeController.to.screenHeight * 0.01,
+                ),
+                const PostCard(),
+                Container(
+                  height: SizeController.to.screenHeight * 0.01,
+                ),
+                const PostCard(),
+                Container(
+                  height: SizeController.to.screenHeight * 0.01,
+                ),
+                const PostCard(),
+                Container(
+                  height: SizeController.to.screenHeight * 0.01,
+                ),
+                const PostCard(),
+              ],
+            ),
+          ),
         ),
-        child: const Icon(
-          SFSymbols.pencil,
-          color: AppColor.backgroundColor,
+        floatingActionButton: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColor.objectColor,
+            shape: const CircleBorder(),
+            padding: const EdgeInsets.all(15),
+          ),
+          child: const Icon(
+            SFSymbols.pencil,
+            color: AppColor.backgroundColor,
+          ),
         ),
-      ),
-    );
+        bottomNavigationBar: const BottomBar());
   }
 }
---
+++ dplanner/lib/pages/post_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sfsymbols/flutter_sfsymbols.dart';
+import 'package:get/get.dart';
+
+import '../controllers/size.dart';
+import '../style.dart';
+
+class PostContent extends StatefulWidget {
+  const PostContent({super.key});
+
+  @override
+  State<PostContent> createState() => _PostContentState();
+}
+
+class _PostContentState extends State<PostContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60.0),
+        child: AppBar(
+          backgroundColor: AppColor.backgroundColor,
+          leadingWidth: SizeController.to.screenWidth * 0.2,
+          leading: IconButton(
+              onPressed: () {
+                Get.back();
+              },
+              icon: const Icon(SFSymbols.chevron_left)),
+          title: const Text(
+            "게시글",
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+          ),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
