c448c36a76db6a45684d7d99489abd6851ecfa76
JINOJ37 <jinoj0423@gmail.com>
2024-03-11 23:26:26 +0900

    :sparkles: Feat : 스낵바 쌓임 방지 (#32)
    기존 스낵바 닫아서 스낵바 빠르게 전환 구현

--- dplanner/lib/widgets/snack_bar.dart
+++ dplanner/lib/widgets/snack_bar.dart
@@ -3,8 +3,15 @@ import 'package:get/get.dart';
 import '../style.dart';
 
 void snackBar({required String title, required String content}) {
+  // 기존 스낵바 닫기
+  if (Get.isSnackbarOpen) {
+    Get.closeCurrentSnackbar();
+  }
+
   Get.snackbar(title, content,
       colorText: AppColor.textColor,
       backgroundColor: AppColor.backgroundColor,
-      snackPosition: SnackPosition.BOTTOM);
+      snackPosition: SnackPosition.BOTTOM,
+      animationDuration: const Duration(milliseconds: 200),
+      isDismissible: true);
 }
