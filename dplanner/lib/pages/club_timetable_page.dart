44d2c50bd28a42458e08ec7cc5d1827cdbad1b6b
JINOJ37 <jinoj0423@gmail.com>
2024-05-03 15:59:59 +0900

    :sparkles: Feat : 예약 관리 권한 기능 설정 (#81)
    예약 시간표 (탭1)에서의 기능 부여

--- dplanner/lib/pages/club_timetable_page.dart
+++ dplanner/lib/pages/club_timetable_page.dart
@@ -601,7 +601,11 @@ class _ClubTimetablePageState extends State<ClubTimetablePage> {
                 return const ErrorPage();
               } else {
                 return Visibility(
-                  visible: MemberController.to.clubMember().role == "ADMIN",
+                  visible: (MemberController.to.clubMember().role == "ADMIN" ||
+                      MemberController.to
+                          .clubMember()
+                          .clubAuthorityTypes!
+                          .contains("SCHEDULE_ALL")),
                   replacement: ElevatedButton(
                     onPressed: () {
                       if (selectedValue!.notice == "") {
@@ -852,8 +856,12 @@ class _ClubTimetablePageState extends State<ClubTimetablePage> {
                             child: Text(
                               reservation != null &&
                                       reservation.status == "REQUEST" &&
-                                      MemberController.to.clubMember().role ==
-                                          "ADMIN"
+                                      (MemberController.to.clubMember().role ==
+                                              "ADMIN" ||
+                                          MemberController.to
+                                              .clubMember()
+                                              .clubAuthorityTypes!
+                                              .contains("SCHEDULE_ALL"))
                                   ? "예약 요청"
                                   : reservation?.status == "REQUEST"
                                       ? "승인 대기중"
@@ -1509,6 +1517,10 @@ class _ClubTimetablePageState extends State<ClubTimetablePage> {
                                       reservation.returned &&
                                       (MemberController.to.clubMember().role ==
                                               "ADMIN" ||
+                                          MemberController.to
+                                              .clubMember()
+                                              .clubAuthorityTypes!
+                                              .contains("SCHEDULE_ALL") ||
                                           reservation.clubMemberId ==
                                               MemberController.to
                                                   .clubMember()
@@ -2760,8 +2772,12 @@ class _ClubTimetablePageState extends State<ClubTimetablePage> {
                         visible: reservation?.clubMemberId ==
                             MemberController.to.clubMember().id,
                         replacement: Visibility(
-                          visible:
-                              MemberController.to.clubMember().role == "ADMIN",
+                          visible: (MemberController.to.clubMember().role ==
+                                  "ADMIN" ||
+                              MemberController.to
+                                  .clubMember()
+                                  .clubAuthorityTypes!
+                                  .contains("SCHEDULE_ALL")),
                           child: Visibility(
                             visible: reservation?.status == "REQUEST",
                             replacement: NextPageButton(
