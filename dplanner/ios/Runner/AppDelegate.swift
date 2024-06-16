1b947a5c4bea13db4628855af25185f5ca44dd14
impala <dla4051@naver.com>
2024-06-16 22:36:40 +0900

    tmp: 카카오 로그인 앱연동 임시저장
    - 추후 info.plist에 <string>kakaokompassauth</string> 추가

--- dplanner/ios/Runner/AppDelegate.swift
+++ dplanner/ios/Runner/AppDelegate.swift
@@ -1,34 +1,40 @@
 import UIKit
 import Flutter
 import NaverThirdPartyLogin
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        }
+        
+        GeneratedPluginRegistrant.register(with: self)
+        
+        if let appKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_API_KEY") as? String {
+            KakaoSDK.initSDK(appKey: appKey)
+        }
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-
-//네이버 로그인 설정 추가하면서 추가한 override func
-override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-    var applicationResult = false
-    if (!applicationResult) {
-       applicationResult = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+    
+    
+    //네이버 로그인 설정 추가하면서 추가한 override func
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        if url.absoluteString.hasPrefix("kakao"){
+            super.application(app, open:url, options: options)
+            return true
+        } else if url.absoluteString.contains("thirdPartyLoginResult") {
+            NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+            return true
+        } else {
+            return true
+        }
     }
-    // if you use other application url process, please add code here.
-
-    if (!applicationResult) {
-       applicationResult = super.application(app, open: url, options: options)
-    }
-    return applicationResult
-}
 }
