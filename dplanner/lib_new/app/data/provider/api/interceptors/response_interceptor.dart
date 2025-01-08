eaa523d1978dab0de19aaf841e3acc240779a0b3
impala <dla4051@naver.com>
2025-01-09 00:07:24 +0900

    feature: 400번대, 500번대 에러 응답시 인터셉터에서 예외를 던져 정상 로직을 타지 않도록 처리
    - 추후 상위 레이어(viewModel or view)에서 공통으로 예외를 잡아 처리하도록(스낵바) 구현 필요

--- dplanner/lib_new/app/data/provider/api/interceptors/response_interceptor.dart
+++ dplanner/lib_new/app/data/provider/api/interceptors/response_interceptor.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:get/get_connect/http/src/exceptions/exceptions.dart';
 import 'package:get/get_connect/http/src/request/request.dart';
 import 'package:get/get_connect/http/src/response/response.dart';
 
@@ -10,16 +11,23 @@ import '../../../model/common_response.dart';
 FutureOr<dynamic> commonResponseBodyBindingInterceptor(
     Request request, Response response) async {
   if (response.hasError) {
+    var responseBody = CommonResponseBody.fromJson(response.body);
+
     if (kDebugMode) {
-      var responseBody = CommonResponseBody.fromJson(response.body);
+      // 디버그 모드 에서만 로깅
       print("[API CALL FAIL] ${request.url} ${responseBody.message} ");
     }
 
     if (response.request!.url.toString().endsWith("/auth/refresh")) {
-      // /auth/refresh 요청이 실패하면 throw
+      // /auth/refresh 요청이 실패시 throw
       throw TokenRefreshFailedException();
     }
+
+    // 400번대, 500번대 에러 -> GetHttpException 예외 throw 후 상위 레이어에서 catch 해서 공통처리
+    throw GetHttpException(responseBody.message ?? "API Call Failed");
   }
+
+  // 성공한 경우 response 변환
   return CommonResponse.from(response);
 }
 
